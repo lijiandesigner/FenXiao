9fb23570e07bb5b4cd6d546db561d2c1c9d99ba5
keke <1285404430@qq.com>
2017-11-21 16:46:16 +0800

    上下级关系（Guanxi)       会员基本信息（Vipmess)     其他的是我上次误删的，已重新获取

---
+++ application/admin/controller/Guanxi.php
@@ -0,0 +1,59 @@
+<?php
+namespace app\admin\Controller;
+use think\Controller;
+use think\Session;
+use think\Db;
+class Guanxi extends Controller
+{	
+	
+	public function index(){
+		$arr=Array();
+		$arr['ce']="ss";
+		if(input()){
+			$arr['method']=1;
+			
+			$ar=Db::name('vip_user')->where(['ID'=>input('id')])->select();
+			//如果存在该用户
+			if($ar){
+				//返回他所有的上、下级
+				$PID=$ar[0]['PID'];
+				$ID=$ar[0]['ID'];
+				$PID==0?$arr['jibie']=1:$arr['jibie']=0;
+
+				for($a=0;$a<3;$a++){
+					if($PID==0){
+						
+						break;
+					}else{
+						$ar1=Db::name("vip_user")->where(['ID'=>$PID])->select();
+						$PID=$ar1[0]['PID'];
+
+						$arr['lev'.$a]=$ar1;
+						if($PID==0){
+							break;
+						}
+					}
+				}
+				//返回他所有的下级
+				for($a=0;$a<3;$a++){
+				
+						$ar1=Db::name("vip_user")->where(['PID'=>$ID])->select();
+						if($ar1){
+							$ID=$ar1[0]['ID'];
+							$arr['xia'.$a]=$ar1;
+						}else{
+							$arr['xia']=0;
+							break;
+						}
+				}
+			}else{
+				$arr['user']=0;
+			}
+		}else{
+			$arr['method']=0;
+		}
+
+		return json_encode($arr);
+	}
+
+}
---
+++ application/admin/controller/Vipmess.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\admin\Controller;
+use think\Controller;
+use think\Db;
+class Vipmess extends Controller
+{
+	public function index(){
+		$arr=Array();
+		$re=Db::name("vip_user")->select();
+		if($re){
+			$arr['data']=$re;
+		}else{
+			$arr['shuju']=0;
+		}
+		return json_encode($arr);
+	}
+}
